037c4b4897e4a54add85b1661e798f7fe87985b2
eibongneg <145131934+wordzseven@users.noreply.github.com>
2025-11-14 10:28:07 +0300

    Update LAB1.py

--- LAB1.py
+++ LAB1.py
@@ -1,5 +1,9 @@
-o = open('sport.txt', mode='r')
-lines, sports = [i.split('\t') for i in o.readlines()], {}
+import requests
+
+lines, sports = [i.split('\t') for i in requests.get('https://dfedorov.spb.ru/python3/sport.txt').content.decode('cp1251').split('\n')], {}
+for i in lines:
+    if i == ['']:
+        continue
 for i in lines:
     if i[3].replace(' ', '').split(',') == ['']:  # фильтр испорченных данны, неясным образом оказавшихся в файле
         continue
@@ -8,4 +12,5 @@ for i in lines:
             sports[j] += 1
         else:
             sports[j] = 1
+
 print(*sorted(sports.items(), key=lambda item: item[1], reverse=True)[:3])
